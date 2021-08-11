a68408d4f232c565a7e4cc95dc9da52030f12f75
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-08-11 09:34:50 +0300

    update

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,40 +5,61 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
     public static void main(String[] args) {
         {
+            ArrayList<User> users = new ArrayList<>();
             try {
                 ServerSocket serverSocket = new ServerSocket(8188); // Создаём серверный сокет
                 System.out.println("Сервер запущен");
-                while (true) { // Бесконечный цикл для ожидания родключения клиентов
+                while (true){ // Бесконечный цикл для ожидания родключения клиентов
                     Socket socket = serverSocket.accept(); // Ожидаем подключения клиента
                     System.out.println("Клиент подключился");
-                    DataInputStream in = new DataInputStream(socket.getInputStream()); // Поток ввода
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
-                    out.writeUTF("Добро пожаловать на сервер");
-                    Thread client = new Thread(new Runnable() {
-
+                    User currentUser = new User(socket);
+                    users.add(currentUser);
+                    DataInputStream in = new DataInputStream(currentUser.getSocket().getInputStream()); // Поток ввода
+                    DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream()); // Поток вывода
+                    Thread thread = new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            while (true) {
-                                String request = null; // Ждём сообщение от пользователя
-                                try {
-                                    request = in.readUTF();
-                                    out.writeUTF(request.toUpperCase()); // Отправляем сообщение пользователю
-                                } catch (IOException e) {
-                                    e.printStackTrace();
+                            try {
+                                out.writeUTF("Добро пожаловать на сервер");
+                                out.writeUTF("Введите ваше имя: ");
+                                String userName = in.readUTF(); // Ожидаем имя от клиента
+                                currentUser.setUserName(userName);
+                                for (User user : users) {
+                                    DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                    out.writeUTF(currentUser.getUserName()+" присоединился к беседе");
                                 }
-
+                                while (true){
+                                    String request = in.readUTF(); // Ждём сообщение от пользователя
+                                    System.out.println(currentUser.getUserName()+": "+request);
+                                    for (User user : users) {
+                                        if(users.indexOf(user) == users.indexOf(currentUser)) continue;
+                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                        out.writeUTF(currentUser.getUserName()+": "+request);
+                                    }
+                                }
+                            }catch (IOException e){
+                                users.remove(currentUser);
+                                for (User user : users) {
+                                    try {
+                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                        out.writeUTF(currentUser.getUserName()+" покинул чат");
+                                    } catch (IOException ioException) {
+                                        ioException.printStackTrace();
+                                    }
+                                }
                             }
                         }
                     });
-                    client.start();
+                    thread.start();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-}//ффф
+}
--- src/сlient/Client.java
+++ src/сlient/Client.java
@@ -15,12 +15,29 @@ public class Client {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
             String response = in.readUTF(); // Читаем ответ от сервера
             System.out.println(response);
+            response = in.readUTF();
+            System.out.println(response);
             Scanner scanner = new Scanner(System.in);
+            out.writeUTF(scanner.nextLine()); // Ожидаем ввод имени
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String response;
+                    while (true){ // Бесконечно ожидаем сообщение от сервера
+                        try {
+                            response = in.readUTF(); // Читаем ответ от сервера
+                            System.out.println(response); // Печатаем ответ от сервера на экран
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
             while (true){
                 String consoleText = scanner.nextLine(); // Ждём сообщение от пользователя
                 out.writeUTF(consoleText); // Отправляем сообщение на сервер
-                response = in.readUTF(); // Читаем ответ от сервера
-                System.out.println("Ответ сервера: "+response); // Печатаем ответ от сервера на экран
+
             }
         } catch (IOException e) {
             e.printStackTrace();
