b5c74f99fc36074be5bd61056b2f7fb150ef2578
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-08-11 15:04:33 +0300

    проверка на уникальность имени, личные сообщения

--- src/server/Server.java
+++ src/server/Server.java
@@ -14,7 +14,7 @@ public class Server {
             try {
                 ServerSocket serverSocket = new ServerSocket(8188); // Создаём серверный сокет
                 System.out.println("Сервер запущен");
-                while (true){ // Бесконечный цикл для ожидания родключения клиентов
+                while (true) { // Бесконечный цикл для ожидания родключения клиентов
                     Socket socket = serverSocket.accept(); // Ожидаем подключения клиента
                     System.out.println("Клиент подключился");
                     User currentUser = new User(socket);
@@ -28,30 +28,29 @@ public class Server {
                                 out.writeUTF("Добро пожаловать на сервер");
                                 out.writeUTF("Введите ваше имя: ");
                                 String userName = in.readUTF(); // Ожидаем имя от клиента
+
+                                while (checkFreeName(users, userName)) { // Проверяем, свободно ли имя
+                                    out.writeUTF("Имя: " + userName + " занято, попробуйте ввести другое");
+                                    userName = in.readUTF();
+                                }
                                 currentUser.setUserName(userName);
+
                                 for (User user : users) {
                                     DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                    out.writeUTF(currentUser.getUserName()+" присоединился к беседе");
+                                    out.writeUTF(currentUser.getUserName() + " присоединился к беседе");
                                 }
-                                while (true){
+                                while (true) {
                                     String request = in.readUTF(); // Ждём сообщение от пользователя
-                                    System.out.println(currentUser.getUserName()+": "+request);
-                                    for (User user : users) {
-                                        if(users.indexOf(user) == users.indexOf(currentUser)) continue;
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName()+": "+request);
+                                    System.out.println(currentUser.getUserName() + ": " + request);
+                                    if (request.startsWith("/m ")) {
+                                        String replacedRequest = request.replaceFirst("/m ", "");
+                                        sendPrivateMessage(replacedRequest, users, currentUser);
+                                    } else {
+                                        sendPublicMessage(request, users, currentUser);
                                     }
                                 }
-                            }catch (IOException e){
-                                users.remove(currentUser);
-                                for (User user : users) {
-                                    try {
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName()+" покинул чат");
-                                    } catch (IOException ioException) {
-                                        ioException.printStackTrace();
-                                    }
-                                }
+                            } catch (IOException e) {
+                                userExit(users, currentUser);
                             }
                         }
                     });
@@ -61,5 +60,71 @@ public class Server {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void sendPublicMessage(String request, ArrayList<User> users, User currentUser) throws IOException {
+        for (User user : users) {
+            if (users.indexOf(user) == users.indexOf(currentUser)) continue;
+            DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+            out.writeUTF(currentUser.getUserName() + ": " + request);
+        }
+    }
+
+    private static void sendPrivateMessage(String request, ArrayList<User> users, User currentUser) throws IOException {
+        User recipient = null;
+        String[] splitted = request.split(" ", 2);
+        String name = "";
+        String message = "";
+        DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream());
+        try {
+            name = splitted[0];
+            message = splitted[1];
+        } catch (IndexOutOfBoundsException ex) {
+            out.writeUTF("Сообщение введено не корректно");
+        }
+
+        for (User user : users) {
+            if (user.getUserName().equals(name)) {
+                recipient = user;
+                break;
+            }
+        }
+        if (recipient != null) {
+            DataOutputStream recipientOut = new DataOutputStream(recipient.getSocket().getOutputStream());
+            recipientOut.writeUTF(currentUser.getUserName() + ": " + message);
+        } else {
+            out.writeUTF("Пользователь с именем: " + name + " отсутствует");
+        }
+
+
+//        for (User user : users) {
+//            if (users.indexOf(user) == users.indexOf(currentUser)) continue;
+//            DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+//            out.writeUTF(currentUser.getUserName() + ": " + request);
+//        }
+
+    }
+
+
+    private static void userExit(ArrayList<User> users, User currentUser) {
+        users.remove(currentUser);
+        for (User user : users) {
+            try {
+                DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                out.writeUTF(currentUser.getUserName() + " покинул чат");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean checkFreeName(ArrayList<User> users, String name) {
+        for (User user : users
+        ) {
+            if (user.getUserName() != null && user.getUserName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
